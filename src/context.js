46b496ce3469774270e61fef20bc651806f27dcd
Aatif Ali <aatifwarraich10@gmail.com>
2022-10-03 12:01:33 +0500

    add ,sub and total amount funcation added

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer, useState } from "react";
+import React, { useContext, useEffect, useReducer, useState } from "react";
 import cartItem from "./data";
 import reducer from "./reducer";
 const url = "https://course-api.com/react-useReducer-cart-project";
@@ -14,10 +14,34 @@ const initialState = {
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  // Clear Item funcation
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+  // remove one item funcataion
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+  // increment Item
+  const add = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  // descrement item
+  const sub = (id) => {
+    dispatch({ type: "DECREMENT", payload: id });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "TOTAL" });
+  }, [state.cart]);
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
+        removeItem,
+        add,
+        sub,
       }}
     >
       {children}
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,53 @@
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let addition = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: addition };
+  }
+  if (action.type === "DECREMENT") {
+    let subs = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: subs };
+  }
+  if (action.type === "TOTAL") {
+    let { total, amount } = state.cart.reduce(
+      (total, cartItem) => {
+        const { price, amount } = cartItem;
+        let totalAmount = price * amount;
+        total.total += totalAmount;
+        total.amount += amount;
+        return total;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
   return state;
 };
 
